bf80c931fb89a549453912a83f2867dad8835570
SHIVAM KUMAR VARSHNEY <59953764+SKV-programmer@users.noreply.github.com>
2020-12-01 12:35:09 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,65 +1,16 @@
-document.addEventListener("keydown",function(e) {
-    if(e.code == "KeyA"){
-        console.log('The \'A\' key is pressed.');
-        let audio = new Audio("assets/A.mp3");
-        audio.play();
-    }
-    else if(e.code == "KeyS"){
-        console.log('The \'S\' key is pressed.');
-        let audio = new Audio("assets/S.mp3");
-        audio.play();
-    }
-    else if(e.code == "KeyD"){
-        console.log('The \'D\' key is pressed.');
-        let audio = new Audio("assets/D.mp3");
-        audio.play();
-    }
-    else if(e.code == "KeyF"){
-        console.log('The \'F\' key is pressed.');
-        let audio = new Audio("assets/F.mp3");
-        audio.play();
-    }
-    else if(e.code == "KeyG"){
-        console.log('The \'G\' key is pressed.');
-        let audio = new Audio("assets/G.mp3");
-        audio.play();
-    }
-    else if(e.code == "KeyH"){
-        console.log('The \'H\' key is pressed.');
-        let audio = new Audio("assets/H.mp3");
-        audio.play();
-    }
-    else if(e.code == "KeyJ"){
-        console.log('The \'J\' key is pressed.');
-        let audio = new Audio("assets/J.mp3");
-        audio.play();
-    }
-    else if(e.code == "KeyW"){
-        console.log('The \'W\' key is pressed.');
-        let audio = new Audio("assets/W.mp3");
-        audio.play();
-    }
-    else if(e.code == "KeyE"){
-        console.log('The \'E\' key is pressed.');
-        let audio = new Audio("assets/E.mp3");
-        audio.play();
-    }
-    else if(e.code == "KeyT"){
-        console.log('The \'T\' key is pressed.');
-        let audio = new Audio("assets/T.mp3");
-        audio.play();
-    }
-    else if(e.code == "KeyY"){
-        console.log('The \'Y\' key is pressed.');
-        let audio = new Audio("assets/Y.mp3");
-        audio.play();
-    }
-    else if(e.code == "KeyU"){
-        console.log('The \'U\' key is pressed.');
-        let audio = new Audio("assets/U.mp3");
-        audio.play();
-    }
-    else{
-        console.log('Warning !! wrong key pressed.');
-    }
-    });
+let whiteKeys = ["KeyA", "KeyS", "KeyD", "KeyF", "KeyG", "KeyH", "KeyJ"]
+let blackKeys = ["KeyW", "KeyE", "KeyT", "KeyY", "KeyU"]
+
+
+document.addEventListener("keydown", function (e) {
+    if (whiteKeys.includes(e.code)) {
+        let pianokey = new Audio("assets/" + e.code.replace("Key", "") + ".mp3");
+        pianokey.play();
+        console.log("The '" + e.code.replace("Key", "") + "' key is pressed");
+    } else if (blackKeys.includes(e.code)) {
+        let pianokey = new Audio("assets/" + e.code.replace("Key", "") + ".mp3");
+        pianokey.play();
+        console.log("The '" + e.code.replace("key", "") + "' key is pressed");
+    } else console.log("The '" + e.code.replace("Key", "") + "' key is not on the piano");
+
+});
